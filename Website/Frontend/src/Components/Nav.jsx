9182bff2df904dd8598f989da8dac5235ba558e9
Majorplaydev <victorayoonileere@gmail.com>
2023-10-24 15:19:28 +0100

    nav structure commit

--- Website/Frontend/src/Components/Nav.jsx
+++ Website/Frontend/src/Components/Nav.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <nav className=" font-lora flex  flex-col justify-evenly items-start uppercase h-screen absolute ">
+        <nav className=" font-poppins flex flex-col justify-evenly items-start w-[15vw] bg-primarytwo shadow-md leading-7 text-xl h-screen absolute pl-7 ">
             <Link to="/">Home</Link>
             <Link to="/myfiles">My Files</Link>
             <Link to="/starred">Starred</Link>
--- Website/Frontend/tailwind.config.js
+++ Website/Frontend/tailwind.config.js
@@ -9,7 +9,6 @@ export default {
       xs: ['12px', '16px'],
       sm: ['14px', '20px'],
       base: ['16px', '19.5px'],
-      lg: ['18px', '21.94px'],
       xl: ['20px', '24.38px'],
       '2xl': ['24px', '29.26px'],
       '3xl': ['28px', '50px'],
@@ -29,13 +28,7 @@ export default {
         "primary-text": "#757897",
         "fill-color": "#EBF2FF"
       },
-      boxShadow: {
-        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
-      },
-      backgroundImage: {
-        'hero': "url('assets/images/collection-background.svg')",
-        'card': "url('assets/images/thumbnail-background.svg')",
-      },
+    
       screens: {
         "wide": "1440px"
       }
